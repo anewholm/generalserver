014db1c5d1559967bf428932c084caaebe4d3bd7
anewholm <annesley_newholm@yahoo.it>
2023-05-02 17:58:04 +0100

    README.md

---
+++ src/installations/libxslt-rr/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/usr/local/lib"
+XSLT_LIBS="-lxsltrr -L/home/sanchez/Software/generalserver/src/installations/libxml2-rr -L/usr/local/lib -lxml2rr -lpthread -lm -lm"
+XSLT_INCLUDEDIR="-I/usr/local/include"
+MODULE_VERSION="xslt-1.1.26"
